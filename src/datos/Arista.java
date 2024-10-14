dde624d1d684d04acc9bbd89ce11f55e0e000a40
Usuario <Usuario@DESKTOP-AL8VFSB>
2024-10-13 22:06:17 -0300

    Se borro el MapaAnterior y se agregaron Test para la creacion de aristas

--- src/datos/Arista.java
+++ src/datos/Arista.java
@@ -5,16 +5,68 @@ public class Arista {
 	private Vertice verticeDestino;
 	private double peso; // es la probabilidad de que el enemigo intercepte esta arista
 	
-	public Arista (Vertice verticeOrigen, Vertice verticeDestino, double peso) {
+	public Arista(Vertice verticeOrigen, Vertice verticeDestino, double peso) {
+		revisarEntradaCorrectaDeValores(verticeOrigen, verticeDestino, peso);
 		this.verticeOrigen = verticeOrigen;
 		this.verticeDestino = verticeDestino;
 		this.peso = peso;
 	}
 	
 	
+	private void revisarEntradaCorrectaDeValores(Vertice verticeOrigen, Vertice verticeDestino, double peso) {
+		if(!(pesoIngresadoEsCorrecto(peso))) {
+			throw new RuntimeException("El valor ingresado debe estar entre 0.0 y 1.0");
+		}
+		if(!(verticesIngresadosSonCorrectos(verticeOrigen, verticeDestino))) {
+			throw new RuntimeException("La Arista tiene que tener vertices de destino y origen distintos");
+		}
+		
+		//TAMBIEN DEBERIA VERIFICAR QUE NO SE REPITAN LAS ARISTAS CON UN CICLO FOR O ALGO QUE CONTROLE Y PASE POR TODAS LAS ARISTAS PARA QUE NO SE REPITAN
+	}
+	
+	
+	
+	private boolean pesoIngresadoEsCorrecto(double peso) {
+		if(peso < 0.0 || peso > 1.0) {
+			return false;
+		}
+		return true;
+	}
+	private boolean verticesIngresadosSonCorrectos(Vertice verticeOrigen, Vertice verticeDestino) {
+		if(verticeOrigen == null || verticeDestino == null) {
+			return false;
+		}
+		if(verticeOrigen.equals(verticeDestino)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) {
+			return false;
+		}
+		if(!(obj instanceof Arista)) {
+			return false;
+		}
+		Arista aux = (Arista) obj;
+		return aux.getPeso() == this.getPeso() && aux.getVerticeOrigen() == this.getVerticeOrigen() && aux.getVerticeDestino() == this.getVerticeDestino();
+	}
+	
+	
 	
 	public boolean ExisteUnion() {			
 		return true;
+	}
+	
+	public Vertice getVerticeOrigen() {
+		return this.verticeOrigen;
+	}
+	public Vertice getVerticeDestino() {
+		return this.verticeDestino;
 	}
 	
 	public double getPeso() {
--- src/datos/AristaTest.java
+++ src/datos/AristaTest.java
@@ -1,8 +1,33 @@
 package datos;
 
 import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class AristaTest {
+	Vertice verticeGenerico;
+	Vertice verticeNull;
+	
+	
+	@Before
+	public void seUp() {
+		BigDecimal coordX = BigDecimal.valueOf(-34.0000);
+		BigDecimal coordY = BigDecimal.valueOf(32.213);
+		Vertice verticeGenerico = new Vertice("Vertice", coordX, coordY);
+		Vertice verticeNull = null;
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void crearAristaOrigenNuloDevuelveError() {
+		Arista nueva = new Arista(verticeNull, verticeGenerico, 0.5);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void crearAristaDestinoNuloDevuelveError() {
+		Arista nueva = new Arista(verticeGenerico, verticeNull, 0.5);
+	}
 	
 }
