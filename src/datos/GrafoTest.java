4db753c89f93da3e4bb9435d86fb071e788b2106
Usuario <Usuario@DESKTOP-AL8VFSB>
2024-10-14 13:16:48 -0300

    Se agrego la clase GrafoTest junto con varios Test Unitarios

---
+++ src/datos/GrafoTest.java
@@ -0,0 +1,36 @@
+package datos;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+
+public class GrafoTest {
+	
+	@Test
+	public void seIniciaConGrafoVacio() {
+		Grafo grafo = new Grafo();
+		assertEquals(true,grafo.getAdjList().isEmpty());
+	}
+	
+	@Test
+	public void seAgregaVerticeCorrectamente() {
+		Vertice vertice = new Vertice("Espia 1", BigDecimal.valueOf(-32.012), BigDecimal.valueOf(12.345));
+		Grafo grafo = new Grafo();
+		grafo.agregarVertice(vertice);
+		
+		assertEquals(1, grafo.getAdjList().size());
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void agregarDosVecesElMismoVerticeArrojaError() {
+		Vertice vertice = new Vertice("Espia 1", BigDecimal.valueOf(-32.012), BigDecimal.valueOf(12.345));
+		Vertice vertice2 = new Vertice("Espia 1", BigDecimal.valueOf(-32.012), BigDecimal.valueOf(12.345));
+		Grafo grafo = new Grafo();
+		grafo.agregarVertice(vertice);
+		grafo.agregarVertice(vertice2);
+	}
+
+}
